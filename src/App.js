d68b5c6e6ef7b64637e1ded4a2ddb4630dcc8f5a
Oleksii Mytko <mytko.oleksii@gmail.com>
2023-05-21 00:23:28 +0200

    lessons 7

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import {Link, Navigate, Route, Routes} from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Blog from "./pages/Blog";
@@ -12,36 +11,41 @@ import EditPost from "./pages/EditPost";
 import Login from "./pages/Login";
 import RequireAuth from "./hoc/RequireAuth";
 import AuthProvider from "./hoc/AuthProvider";
+import React from "react";
 
 
 function App() {
     return (
         <AuthProvider>
-        <div className="App">
+            <div className="App">
 
-            <div>
-                <Routes>
-                    <Route path={'/'} element={<Layout/>}>
-                        <Route index element={<Home/>}/>
-                        <Route path='/about' element={<About/>}/>
-                        <Route path='/about-us'
-                               element={<Navigate to={'/about'} replace/>}/> {/*// Navigate -переадресация */}
-                        <Route path={'/posts'} element={<Blog/>}/>
-                        <Route path={'/posts/:id'} element={<Single/>}/>
-                        <Route path={'/posts/:id/edit'} element={<EditPost/>}/>
-                        <Route path={'/posts/new'} element={
-                            <RequireAuth>
-                                <Creator/>
-                            </RequireAuth>
-                        }/>
-                        <Route path='/login' element={<Login/>}/>
+                <div>
+                    <Routes>
+                        <Route path={'/'} element={<Layout/>}>
+                            <Route index element={<Home/>}/>
+                            <Route path='/about/*' element={<About/>}>
+                               {/* // Вложенніе роути*/}
+                                <Route path={'contacts'} element={<p>Our contacts</p>}/>
+                                <Route path={'team'} element={<p>Our team</p>}/>
+                            </Route>
+                            <Route path='/about-us'
+                                   element={<Navigate to={'/about'} replace/>}/> {/*// Navigate -переадресация */}
+                            <Route path={'/posts'} element={<Blog/>}/>
+                            <Route path={'/posts/:id'} element={<Single/>}/>
+                            <Route path={'/posts/:id/edit'} element={<EditPost/>}/>
+                            <Route path={'/posts/new'} element={
+                                <RequireAuth>
+                                    <Creator/>
+                                </RequireAuth>
+                            }/>
+                            <Route path='/login' element={<Login/>}/>
 
-                        <Route path={'*'} element={<Notfound/>}/>
-                    </Route>
-                </Routes>
-                <p> App. Get started with React-Router 6 . </p>
+                            <Route path={'*'} element={<Notfound/>}/>
+                        </Route>
+                    </Routes>
+                    <p> App. Get started with React-Router 6 . </p>
+                </div>
             </div>
-        </div>
         </AuthProvider>
     );
 }
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import {Link, Outlet} from "react-router-dom";
 
-function About(props) {
+function About() {
     return (
         <div>
           <h1> About</h1>
             Описание всего остального
+            <ul>
+                <li> <Link to={'contacts'}>Contacts</Link></li>
+                <li> <Link to={'team'}>Team</Link></li>
+
+            </ul>
+          {/*  <Routes>
+                <Route path={'contacts'} element={<p>Our contacts</p>} />
+                <Route path={'team'} element={<p>Our team</p>} />
+            </Routes>*/}
+
+            <Outlet/>
 </div>
     );
 }
